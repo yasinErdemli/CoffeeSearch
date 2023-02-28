a992bbe64928fe4ce19300677dfee4aa5260f032
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-02-28 16:01:24 +0300

    create app

---
+++ CoffeeSearch/CoffeeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeSearchApp.swift
+//  CoffeeSearch
+//
+//  Created by Yasin Erdemli on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
